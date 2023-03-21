94a3870c9e1329f42c703277e2f242501aa3dd72
Chris Chuck <chrischuckgithub@gmail.com>
2023-03-20 20:18:53 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button, View, StyleSheet, Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
-export default function App() {
+export default function AssetExample() {
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      exif: true,
+      quality: 1,
+    });
+
+    console.log(JSON.stringify(result.exif, null, 2));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={{ height: 100, width: 100 }}
+        source={require("./exif-test.jpeg")}
+      />
+      <Button title="select image" onPress={pickImage} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 24,
   },
 });
